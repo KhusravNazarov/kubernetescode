a629ba5a48e871eebf941af0e2e5ed1d3f1ebf70
KhusravNazarov <160560088+KhusravNazarov@users.noreply.github.com>
2024-11-12 17:40:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+# app.py
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
